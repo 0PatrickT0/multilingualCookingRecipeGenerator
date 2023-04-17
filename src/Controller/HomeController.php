ab31fcb4a8cd1de7d6457f4bf6f7a15580e55a4b
0PatrickT0 <95145049+0PatrickT0@users.noreply.github.com>
2023-04-18 01:17:50 +0200

    create index.html.twig random picture

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Finder\Finder;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index_picture(): Response
     {
+        $finder = new Finder();
+        $finder->files()->in('images')->name('*.png');
+        $images = [];
+        foreach ($finder as $file) {
+            $images[] = $file->getFilename();
+        }
+        $random_image = $images ? $images[array_rand($images)] : '';
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'random_image' => $random_image,
         ]);
+/*         return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]); */
     }
 }
