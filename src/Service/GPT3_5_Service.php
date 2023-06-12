c34ba9594166a0eb571bd1fb3946d03b26782fa9
0PatrickT0 <95145049+0PatrickT0@users.noreply.github.com>
2023-06-12 22:12:51 +0200

    Edit Service

--- src/Service/GPT3_5_Service.php
+++ src/Service/GPT3_5_Service.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class GPT3_5_Service
 {
@@ -28,54 +29,59 @@ class GPT3_5_Service
     {
         $open_ai_key = $this->parameterBag->get('OPENAI_API_KEY');
 
-        $response = $this->client->request(
-            'POST',
-            'https://api.openai.com/v1/chat/completions',
-            [
-                'headers' => [
-                    'Content-Type' => 'application/json',
-                    'Authorization' => 'Bearer ' . $open_ai_key,
-                ],
-                'json' => [
-                    'model' => 'gpt-3.5-turbo',
-                    'messages' => [
-                        [
-                            'role' => 'user',
-                            'content' => "proposes une recette de cuisine dans la langue utilisée et avec les ingrédients suivants : " . $instructions,
-                        ]
+        try {
+            $response = $this->client->request(
+                'POST',
+                'https://api.openai.com/v1/chat/completions',
+                [
+                    'headers' => [
+                        'Content-Type' => 'application/json',
+                        'Authorization' => 'Bearer ' . $open_ai_key,
                     ],
-                ],
-            ]
-        );
+                    'json' => [
+                        'model' => 'gpt-3.5-turbo',
+                        'messages' => [
+                            [
+                                'role' => 'user',
+                                'content' => "proposes une recette de cuisine saine et légère dans la langue utilisée et avec les ingrédients suivants : " . $instructions,
+                            ]
+                        ],
+                    ],
+                ]
+            );
 
-        switch ($response->getStatusCode()) {
-            case 200:
-                $responseArray = $response->toArray() ?? [];
+            switch ($response->getStatusCode()) {
+                case 200:
+                    $responseArray = $response->toArray() ?? [];
 
-                // Create ChatLog entity and set the question and answer
-                $chatLog = new ChatLog();
-                $chatLog->setQuestion($instructions);
-                $chatLog->setAnswer($responseArray['choices'][0]['message']['content']);
+                    // Create ChatLog entity and set the question and answer
+                    $chatLog = new ChatLog();
+                    $chatLog->setQuestion($instructions);
+                    $chatLog->setAnswer($responseArray['choices'][0]['message']['content']);
 
-                // Link ChatLog to User if user is connected
-                $user = $this->security->getUser();
-                if ($user) {
-                    $chatLog->setUser($user);
-                } else {
-                    $anonymousUser = $this->entityManager->getRepository(User::class)->findOneBy(['username' => 'anonymous']);
-                    $chatLog->setUser($anonymousUser);
-                }
+                    // Link ChatLog to User if user is connected
+                    $user = $this->security->getUser();
+                    if ($user) {
+                        $chatLog->setUser($user);
+                    } else {
+                        $anonymousUser = $this->entityManager->getRepository(User::class)->findOneBy(['username' => 'anonymous']);
+                        $chatLog->setUser($anonymousUser);
+                    }
 
-                $this->entityManager->persist($chatLog);
-                $this->entityManager->flush();
+                    $this->entityManager->persist($chatLog);
+                    $this->entityManager->flush();
 
-                return nl2br($responseArray['choices'][0]['message']['content']);
+                    return nl2br($responseArray['choices'][0]['message']['content']);
 
-                break;
+                    break;
 
-            default:
-                return 'An error has occurred with code: ' . $response->getStatusCode() . ', please try again.';
-                break;
+                default:
+                    return 'An error has occurred with code: ' . $response->getStatusCode() . ', please try again.';
+                    break;
+            }
+        } catch (TransportExceptionInterface $exception) {
+            // Handle transport exception
+            return 'An error has occurred: ' . $exception->getMessage();
         }
     }
 }
